e05312ea9dea0f186f20a3031a55f151e9545f91
sbpark88 <sbpark88@gmail.com>
2022-04-30 18:31:28 +0900

    Add: 포인터 테스트
    정보처리기사 실기 포인터 이해 공부용.

--- C Lang/main.c
+++ C Lang/main.c
@@ -1,14 +1,20 @@
-//
-//  main.c
-//  C Lang
-//
-//  Created by 박새별 on 2022/04/30.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int a[3] = {3,4};
+    
+    int *p = a;
+    
+    printf("%p %p %p \n", a, (a+1), (a+2));
+    
+    printf("%p %p %p \n", &a[0], &a[1], &a[2]);
+    
+    printf("%d %d %d \n", a[0], a[1], a[2]);
+    
+    printf("%d %d %d \n", *a, *(a+1), *(a+2));
+    
+    printf("%d %d %d \n", *p, *(p+1), *(p+2));
+    
     return 0;
 }
