e480d4c1a0be475498cb6ab2274fd18b262b2429
sbpark88 <sbpark88@gmail.com>
2022-04-30 17:52:54 +0900

    Initial Commit

---
+++ C Lang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Lang
+//
+//  Created by 박새별 on 2022/04/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
